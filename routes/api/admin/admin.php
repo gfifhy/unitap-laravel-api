54c6bc8dee1d13cf24230338f1df0bdd46855e9a
James Francis <49358080+gfifhy@users.noreply.github.com>
2023-04-07 05:06:31 +0800

    Removed

--- routes/api/admin/admin.php
+++ routes/api/admin/admin.php
@@ -2,10 +2,12 @@
 
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['role:Admin']], function(){
-    Route::get('/add/student', [AuthController::class,'addStudent'])->name('add.student');
+    Route::post('/add/student', [AuthController::class,'addStudent'])->name('add.student');
+    Route::get('/roles', [RoleController::class, 'index'])->name('index.role');
 });
 
 
