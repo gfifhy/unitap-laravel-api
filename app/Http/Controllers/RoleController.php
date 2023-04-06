9092814f97c72d963852cf26d75a93711a3b6202
James Francis <49358080+gfifhy@users.noreply.github.com>
2023-04-07 05:08:34 +0800

    Added Role Controller

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    public function index(){
+        return Role::all();
+    }
+}
