435eb179d6895bf0c911a4faa2f73b36bf2f90b5
James Francis <49358080+gfifhy@users.noreply.github.com>
2023-04-29 11:17:52 +0800

    Added update, delete, and search function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,7 +11,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    public function login(Request $request){
-
-    }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index() {
+        return Product::whereNull('deleted_at')->where('user_id', auth()->user()->id);
+    }
+
+
 }
