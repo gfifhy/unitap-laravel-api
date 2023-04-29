1819d8fc287c372b5b738a2441290caf4ad85a93
James Francis <49358080+gfifhy@users.noreply.github.com>
2023-04-29 10:42:51 +0800

    added user function routes

--- routes/api/admin/admin.php
+++ routes/api/admin/admin.php
@@ -3,11 +3,15 @@
 
 use App\Http\Controllers\ResourceController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['role:admin']], function(){
-    Route::post('/add/student', [ResourceController::class,'addStudent'])->name('add.student');
-    Route::post('add/staff', [ResourceController::class, 'addStaff'])->name('add.staff');
+    Route::post('/student', [ResourceController::class,'addStudent'])->name('add.student');
+    Route::post('/staff', [ResourceController::class, 'addStaff'])->name('add.staff');
+    Route::get('/users', [UserController::class, 'index'])->name('index.users');
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('show.users');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('archive.users');
     Route::get('/roles', [RoleController::class, 'index'])->name('index.role');
 });
 
