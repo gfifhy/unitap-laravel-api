745cdecd2c8a1d25383488c03159ecd50736c863
James Francis <49358080+gfifhy@users.noreply.github.com>
2023-05-01 12:01:09 +0800

    j

---
+++ database/migrations/2023_05_01_053229_create_location_histories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('location_histories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->char('location_id');
+            $table->char('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('location_histories');
+    }
+};
